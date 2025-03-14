f42c5454d4870b4e083919c9129ab37172d99b86
Henry Nicollas <henrynissicaba@gmail.com>
2025-03-14 20:37:55 -0300

    added animation for logo ticker

--- src/components/sections/LogoTicker.tsx
+++ src/components/sections/LogoTicker.tsx
@@ -1,12 +1,26 @@
+"use client"
+
 import { logoTicker } from "@/utils"
 import Image from "next/image"
+import { motion } from "framer-motion"
 
 export default function LogoTicker(){
     return(
         <div className="py-8 md:py-12 bg-white">
             <div className="container-section">
                 <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
-                    <div className="flex gap-14 flex-none">
+                    <motion.div 
+                        className="flex gap-14 flex-none pr-14"
+                        animate={{
+                            x: "-50%"
+                        }}
+                        transition={{
+                            duration: 20,
+                            repeat: Infinity,
+                            ease: "linear",
+                            repeatType: "loop",
+                        }}
+                    >
                         {logoTicker.map((props, index) => (
                             <Image 
                                 src={props.src} 
@@ -15,7 +29,18 @@ export default function LogoTicker(){
                                 className="h-8 w-auto"
                             />
                         ))}
-                    </div>
+
+                        {/* Second set of logos for animation */}
+
+                        {logoTicker.map((props, index) => (
+                            <Image 
+                                src={props.src} 
+                                alt={props.alt}
+                                key={index}
+                                className="h-8 w-auto"
+                            />
+                        ))}
+                    </motion.div>
                 </div>
             </div>
         </div>
